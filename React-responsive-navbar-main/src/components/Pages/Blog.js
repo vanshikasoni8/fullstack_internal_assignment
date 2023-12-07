ae5616b8bfa3fa5034225cd8078653dda45d45e7
Vanshika soni <106619869+vanshikasoni8@users.noreply.github.com>
2023-12-07 23:41:08 +0530

    Add files via upload

---
+++ React-responsive-navbar-main/src/components/Pages/Blog.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Blog = () => {
+  return (
+    <div>
+      <h1>Let's start blogging</h1>
+    </div>
+  );
+};
---
+++ React-responsive-navbar-main/src/components/Pages/Home.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Home = () => {
+  return (
+    <div>
+      <h1>Vanshika's Area</h1>
+    </div>
+  );
+};
